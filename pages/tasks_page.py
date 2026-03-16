5c0d9ae061f9d5d7c029d2534c9bcb0779c4ee01
Aashrith Murari <156699951+Aashrith2004@users.noreply.github.com>
2026-03-16 14:59:59 +0530

    Update tasks_page.py

--- pages/tasks_page.py
+++ pages/tasks_page.py
@@ -1,40 +1,54 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from pages.base_page import BasePage
-
+import time
 
 class TasksPage(BasePage):
-
     FIRST_EDIT_BUTTON = (By.XPATH, "//table//tbody//tr[1]//button[contains(@class,'btn-outline-primary')]")
-
     STATUS_DROPDOWN = (By.XPATH, "//label[text()='Status']/following::select[1]")
-
     SAVE_BUTTON = (By.XPATH, "//button[contains(.,'Save')]")
-
     STATUS_BADGE = (By.XPATH, "//table//tbody//tr[1]//span[contains(@class,'badge')]")
+    MODAL = (By.XPATH, "//div[contains(@class,'modal') and contains(@class,'show')]")
 
     def click_first_edit(self):
+        # Wait for table to be fully loaded first
+        self.wait.until(
+            EC.visibility_of_element_located(self.FIRST_EDIT_BUTTON)
+        )
         self.click(self.FIRST_EDIT_BUTTON)
-
         self.wait.until(
             EC.visibility_of_element_located(self.STATUS_DROPDOWN)
         )
 
     def change_status(self, status):
+        # Wait for dropdown to be clickable before selecting
+        self.wait.until(
+            EC.element_to_be_clickable(self.STATUS_DROPDOWN)
+        )
         dropdown = Select(self.find(self.STATUS_DROPDOWN))
         dropdown.select_by_value(status)
 
     def save_task(self):
-        self.click(self.SAVE_BUTTON)
-
-        # wait until modal disappears
-        self.wait.until(
-            EC.invisibility_of_element_located(self.STATUS_DROPDOWN)
+        # Wait for save button to be clickable
+        save_btn = self.wait.until(
+            EC.element_to_be_clickable(self.SAVE_BUTTON)
         )
+        # Scroll into view and click
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", save_btn)
+        time.sleep(0.5)
+        save_btn.click()
+
+        # Wait for modal to fully disappear
+        self.wait.until(
+            EC.invisibility_of_element_located(self.MODAL)
+        )
+        # Small buffer for table to refresh
+        time.sleep(1)
 
     def get_status(self):
-        # always re-find the badge to avoid stale element
+        # Always re-find the badge to avoid stale element
         badge = self.wait.until(
             EC.presence_of_element_located(self.STATUS_BADGE)
         )
